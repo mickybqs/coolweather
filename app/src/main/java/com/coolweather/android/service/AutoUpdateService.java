dd0866ba9620ea2bed4fc02b049b13dc7d4b8fbd
unknown <mickyb@foxmail.com>
2018-06-10 16:10:05 +0800

    增加后台自动更新天气的功能

---
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -0,0 +1,116 @@
+package com.coolweather.android.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.coolweather.android.gson.Weather;
+import com.coolweather.android.util.HttpUtil;
+import com.coolweather.android.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class AutoUpdateService extends Service {
+    /**
+     * 在WeatherActivity中激活服务
+     */
+    public AutoUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId) {
+        updateWeather();
+        updateBingPic();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 8*60*1000;  //这是8小时的毫秒数
+        long triggerAtTime = SystemClock.elapsedRealtime()+anHour;
+        Intent i = new Intent(this,AutoUpdateService.class);
+        PendingIntent pi =PendingIntent.getService(this,0,i,0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    /**
+     * 更新天气信息
+     */
+    private void updateWeather() {
+        /**将更新后的数据直接存储到SharedPreferences文件中,
+         * 打开WeatherActivity的时候优先会从SharedPreferences,
+         * 缓存中读取数据.
+         */
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = prefs.getString("weather",null);
+        if (weatherString!= null) {
+            //Parse weather data directly when there is a cache
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            String weatherId = weather.basic.weatherId;
+
+            String weatherUrl = "http://guolin.tech/api/weather?cityid="
+                    +weatherId + "&key=ac3ce8b78d4340279c309e6dde012f00";
+            HttpUtil.sendOKHttpRequest(weatherUrl, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String responseText = response.body().string();
+                    Weather weather = Utility.handleWeatherResponse(responseText);
+                    if (weather != null && "ok".equals(weather.status)) {
+                        SharedPreferences.Editor editor = PreferenceManager.
+                                getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather",responseText);
+                        editor.apply();
+                    }
+
+                }
+            });
+        }
+    }
+
+    /**
+     * Update Bing Daily chart
+     */
+    private void updateBingPic() {
+
+        /**将更新后的数据直接存储到SharedPreferences文件中,
+         * 打开WeatherActivity的时候优先会从SharedPreferences,
+         * 缓存中读取数据.
+         */
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOKHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager
+                        .getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+
+            }
+        });
+    }
+}
